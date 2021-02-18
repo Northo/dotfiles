07a3966b092f7b5f3d0596ac62581d9a7a78498e
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2021-02-18 09:48:41 +0100

    More i3

---
+++ i3/scripts/lock.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+# [[ -f $1 ]] && convert /tmp/screen.png $2 -gravity center -composite -matte /tmp/screen.png
+playerctl pause
+i3lock -i /tmp/screen.png
+rm /tmp/screen.png
